8243236fd079c0bf414be3070377db034583bd21
Michael-H777 <michael-hsi@outlookcom>
2021-03-29 17:59:01 -0400

    fixed decoder_target channel error

--- models/networks.py
+++ models/networks.py
@@ -15,11 +15,10 @@ class classification_cuda(base_model):
         layers = 4
         channel_growth = 4 # 8 won't work zzzz
         
-        loss = {}
         model = encoder(in_channels=3, layers=layers, channel_growth=channel_growth, group_norm=False)
         skip_channels, bottom_channels = model.channels
         model = self.move_model(model)
-        self.structure['encoder'] = model_wrapper(options=options, model=model, loss=loss, name='encoder')
+        self.structure['encoder'] = model_wrapper(options=options, model=model, loss={}, name='encoder')
         
         loss = {'l1': loss_types['l1'], 
                 'l2': loss_types['l2']}
@@ -35,32 +34,41 @@ class classification_cuda(base_model):
         model = self.move_model(model)
         self.structure['classifier'] = model_wrapper(options=options, model=model, loss=loss, name='FC Classifier')
         
-    def set_input(self, data):
-        self.input_data = data[:,0:3,:,:].cuda()
-        self.decode_target = data[:,-2:-1,:,:].cuda()
-        classify_target = data[:,-1:,:,:]
-        classify_target = torch.mean(classify_target, dim=[1,2,3])
-        self.classify_target = torch.unsqueeze(classify_target, dim=1).cuda()
-        
     def classify(self):
         skip, bottom = self.encode_result
         result = [torch.flatten(tensor, start_dim=1) for tensor in [skip, bottom]]
         result = torch.cat(result, dim=1)
         self.classify_result = self.structure['classifier'].forward(result)
+        return None 
         
     def decode(self):
         self.decode_result = self.structure['decoder'].forward(self.encode_result)
+        return None 
     
     def encode(self):
         self.encode_result = self.structure['encoder'].forward(self.input_data)
+        return None 
     
     def forward(self): 
         self.encode()
         self.decode()
         self.classify()
+        return None 
         
     def train(self):
         pass 
     
     def test(self):
-        pass
+        self.encode()
+        self.classify()
+        return self.classify_result
+
+    def set_input(self, data):
+        # batch * (R,G,B,Label) * H * W
+        self.input_data = data[:,0:3,:,:].cuda()
+        self.decode_target = input_data
+        classify_target = data[:,-1:,:,:]
+        classify_target = torch.mean(classify_target, dim=[1,2,3])
+        self.classify_target = torch.unsqueeze(classify_target, dim=1).cuda()
+        return None 
+    
